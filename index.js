bd161194b2a799b9471f392aa86fe903225ded3b
joan bogunya <joanbog97@gmail.com>
2019-03-08 04:43:27 +0100

    noveCommit

--- index.js
+++ index.js
@@ -34,8 +34,19 @@ app.get("/:app_code", function(req, res){
 
 app.post("/:app_code", function(req, res){
 
-    var id = req.params.app_code;
-    db.query("INSERT INTO record (app_code, player, score) VALUES ($1, $2, $3);", [id, req.body.name, req.body.score], onData);
+    if(req.body.name == "" || req.body.score == ""){
+        res.status(422);
+        res.end(["Error 422. It has not been indicated the name of the player or the scores"].join('\n'))
+    }
+    else{
+        var id = req.params.app_code;
+        db.query("INSERT INTO record (app_code, player, score) VALUES ($1, $2, $3);", [id, req.body.name, req.body.score], function(err, data){
+            if(err){
+                res.status(500);
+                res.end(["Error 500"].join('\n'));
+            }
+        });
+    }
 })
 
 app.listen(process.env.PORT || 3000);
